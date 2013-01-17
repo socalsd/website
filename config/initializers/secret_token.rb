ef38aa293f727160db71e09ef5bc071430dbbdc4
Stephen How <stephenhow@mac.com>
2013-01-17 13:30:59 -0800

    Initial snapshot.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socalsd::Application.config.secret_token = '6744d741899cb557cbb4a369c1e055b541fa5117a16b14c7a4217d85c66e1faa12fbafe075b6f8c44b62f5b525640ce7bd0e63448e37fa871414223a5a9d59a5'
